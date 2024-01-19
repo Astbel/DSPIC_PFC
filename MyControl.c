0cb67d1bdba7cad3945bfc764f1a32709769126c
Astbel <67743361+Astbel@users.noreply.github.com>
2024-01-19 22:03:23 +0800

    PSU Portable: 1.新增電流控制器 PID & TYPE 補償 2.前饋控制 這裡欠缺 Vac 取樣 3.PI 控制器在 限幅度這裡邏輯要修正

--- MyControl.c
+++ MyControl.c
@@ -4,11 +4,14 @@
 // 結構體 物件並初始化 電壓環,電流環
 PID Voltage_Loop_PID = {0, 0, 0, 0, 0, 0, 0};
 PID Current_Loop_PID = {0, 0, 0, 0, 0, 0, 0};
+Gain Loop_Gain = {0, 0, 0, 0};
 Type_Compesation Voltage_Loop_2P2Z = {{0}, {0}, {0}, {0}};
 Type_Compesation Current_Loop_2P2Z = {{0}, {0}, {0}, {0}};
+
 // 物件指標建立,只用指標傳遞struct內的值可以節省時間如直接回傳struct會要複製所有的副本
 PID *_Volt_PID;
 PID *_Curr_PID;
+Gain *_Loop_Gain;
 Type_Compesation *_Volt_2P2Z;
 Type_Compesation *_Curr_2P2Z;
 // 初始化用
@@ -18,11 +21,14 @@ void MyControl_Inital(void)
     _Curr_PID = &Current_Loop_PID;
     _Volt_2P2Z = &Voltage_Loop_2P2Z;
     _Curr_2P2Z = &Current_Loop_2P2Z;
+    _Loop_Gain = &Loop_Gain;
 }
 
+/**電壓環**/
 /**
  * @brief 古典控制
  * 電壓環 PI版本 這裡需要新增換檔機制
+ * output Iref PFC而言
  * @param err 目標與當前的誤差量
  */
 inline int32 Voltage_Loop_PI(uint16_t err)
@@ -41,22 +47,22 @@ inline int32 Voltage_Loop_PI(uint16_t err)
         _Volt_PID->Ki_out = 0;
 
     else if ((err < 0) && (_Volt_PID->Kp > 0))
-        _Volt_PID->Kd_out = 0;
+        _Volt_PID->Ki_out = 0;
 
     // 積分上下飽和限制
     if (_Volt_PID->Ki_out > Intergal_MAX)
         _Volt_PID->Ki_out = Intergal_MAX;
 
     else if (_Volt_PID->Ki_out < Intergal_MIN)
-        _Volt_PID->Kd_out = Intergal_MIN;
+        _Volt_PID->Ki_out = Intergal_MIN;
 
     // PI 輸出
     _Volt_PID->Out = _Volt_PID->Kp_out + _Volt_PID->Ki_out;
     /*DPWM 輸出限制檢測*/
-    if (_Volt_PID->Out > DPWM_MAX)
-        _Volt_PID->Out = DPWM_MAX;
-    else if (_Volt_PID->Out < DPWM_MIN)
-        _Volt_PID->Out = DPWM_MIN;
+    if (_Volt_PID->Out > Iref_Limit_Max)
+        _Volt_PID->Out = Iref_Limit_Max;
+    else if (_Volt_PID->Out < Iref_Limit_Min)
+        _Volt_PID->Out = Iref_Limit_Min;
 
     output = _Volt_PID->Out;
 
@@ -68,6 +74,7 @@ inline int32 Voltage_Loop_PI(uint16_t err)
  * 電壓環 Type補償器版本
  * Xn 輸入
  * Yn 輸出
+ * output Iref PFC而言
  * @param err 目標與當前的誤差量
  */
 inline int32 Voltage_Loop_Type(uint16_t err)
@@ -100,4 +107,102 @@ inline int32 Voltage_Loop_Type(uint16_t err)
     output = Voltage_Loop_2P2Z.Yn[0];
 
     return output;
+}
+
+/*電流環*/
+/**
+ * @brief 古典控制
+ * 電流環 PI版本 Iref這裡需要先經過VFF
+ * @param err Iref & Ind的差距
+ * @return uint32_t DPWM的duty
+ */
+inline uint32_t Current_Loop_PI(uint32_t err)
+{
+    uint32_t output;
+
+    // Kp
+    _Curr_PID->Kp_out = _Curr_PID->Kp * err;
+    // Ki
+    _Curr_PID->Ki_out = 0 + _Curr_PID->Ki * err;
+    // 積分遇限削弱 要修正個人覺得思路不對
+    if ((err > 0) && (_Curr_PID->Kp < 0))
+        _Curr_PID->Ki_out = 0;
+
+    else if ((err < 0) && (_Curr_PID->Kp > 0))
+        _Curr_PID->Ki_out = 0;
+
+    // 積分上下飽和限制
+    if (_Curr_PID->Ki_out > Intergal_MAX)
+        _Curr_PID->Ki_out = Intergal_MAX;
+
+    else if (_Curr_PID->Ki_out < Intergal_MIN)
+        _Curr_PID->Ki_out = Intergal_MIN;
+
+    // PI 輸出
+    _Curr_PID->Out = _Curr_PID->Kp_out + _Curr_PID->Ki_out;
+    /*DPWM 輸出限制檢測*/
+    if (_Curr_PID->Out > DPWM_MAX)
+        _Curr_PID->Out = DPWM_MAX;
+    else if (_Curr_PID->Out < DPWM_MIN)
+        _Curr_PID->Out = DPWM_MIN;
+
+    /*DPWM輸出*/
+    output = _Curr_PID->Out;
+
+    return output
+}
+/**
+ * @brief POLE ZERO CANCEL
+ * 電流環 Type補償器版本
+ * Xn 輸入
+ * Yn 輸出
+ * @param err Iref & Ind的差距
+ * @return uint32_t DPWM的duty
+ */
+inline uint32_t Current_Loop_Type(uint16_t err)
+{
+    uint32_t output;
+    /*差分方呈*/
+    Current_Loop_2P2Z.Xn[0] = err;
+
+    Current_Loop_2P2Z.Yn[0] = Current_Loop_2P2Z.Bn[3] * Current_Loop_2P2Z.Xn[3] +
+                              Current_Loop_2P2Z.Bn[2] * Current_Loop_2P2Z.Xn[2] +
+                              Current_Loop_2P2Z.Bn[1] * Current_Loop_2P2Z.Xn[1] +
+                              Current_Loop_2P2Z.Bn[0] * Current_Loop_2P2Z.Xn[0] +
+                              Current_Loop_2P2Z.An[3] * Current_Loop_2P2Z.Yn[3] +
+                              Current_Loop_2P2Z.An[2] * Current_Loop_2P2Z.Yn[2] +
+                              Current_Loop_2P2Z.An[1] * Current_Loop_2P2Z.Yn[1];
+    /*iteration 紀錄上一筆*/
+    for (int8_t i = Orders; i > 0; i--)
+    {
+        Current_Loop_2P2Z.Xn[i] = Current_Loop_2P2Z.Xn[i - 1];
+        Current_Loop_2P2Z.Yn[i] = Current_Loop_2P2Z.Yn[i - 1];
+    }
+
+    /*限制輸出*/
+    if (Current_Loop_2P2Z.Yn[0] > DPWM_MAX)
+        Current_Loop_2P2Z.Yn[0] = DPWM_MAX;
+
+    else if (Current_Loop_2P2Z.Yn[0] < DPWM_MIN)
+        Current_Loop_2P2Z.Yn[0] = DPWM_MIN;
+
+    output = Current_Loop_2P2Z.Yn[0];
+
+    return output;
+}
+/**
+ * @brief 前饋控制 boost 在直流電取交流信號
+ *        整流信號 取樣
+ * @param Boost_Target 電壓環輸出
+ * @Gain  my_gain AC交流增益
+ * @return uint32_t  Iref
+ */
+inline uint32_t Voltage_Feed_Forward(int32 Boost_Target,Gain my_gain)
+{
+    /*Iref*/
+    /*KM Gain 交流信號倍率 整留下取樣*/
+    /* 1/Vrms^2,暫定90Vac下的AC增益,這裡欠缺AC ADC 取樣 */
+    Iref = my_gain.KM_90Vac*Boost_Target;
+
+    return Iref;
 }
--- MyControl.h
+++ MyControl.h
@@ -4,6 +4,9 @@
 // 變數名稱
 #define Orders 3
 
+//Iref 限制間接限制I Limit
+#define Iref_Limit_Max  4096
+#define Iref_Limit_Min    0
 // DPWM limit 暫定解析度
 #define DPWM_MAX 4096
 #define DPWM_MIN 0
@@ -11,6 +14,15 @@
 #define Intergal_MAX 1000
 #define Intergal_MIN 0
 // 結構體申明區
+
+typedef struct 
+{
+    float KM_90Vac;
+    float KM_115Vac;
+    float KM_230Vac;
+    float KM_264Vac;
+}Gain;
+
 
 // PID class
 typedef struct
@@ -41,5 +53,11 @@ extern Type_Compesation Voltage_Loop_2P2Z;
 void MyControl_Inital(void);
 inline int32 Voltage_Loop_PI(uint16_t err);
 inline int32 Voltage_Loop_Type(uint16_t err);
+inline uint32_t Current_Loop_PI(uint32_t err);
+inline uint32_t Current_Loop_Type(uint16_t err);
+inline uint32_t Voltage_Feed_Forward(int32 Boost_Target,Gain my_gain);
+
+
+
 
 #endif
